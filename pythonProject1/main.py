715b3663e8d13f60097abb29cbbeb68cd6bfa439
BuildTools <unconfigured@null.spigotmc.org>
2024-04-09 18:32:23 +0200

    opravy task 2

--- pythonProject1/main.py
+++ pythonProject1/main.py
@@ -1,9 +1,9 @@
 from coffe_maker import Coffee_Maker
 from blender import Blender
 from meat_grinder import Meat_Grinder
-from cruiser import Cruiser
-from frigate import Frigate
-from destroyer import Destroyer
+from cruiser import Cruiser as Cruiser
+from frigate import Frigate as Frigate
+from destroyer import Destroyer as Destroyer
 
 #Task1
 
@@ -12,18 +12,21 @@ coffee2 = Coffee_Maker("Tassimo", 5000, 7)
 coffee3 = Coffee_Maker("Nespresso", 4800, 6)
 
 print(coffee3)
+print()
 
 b1 = Blender("Blender1", 1520, 220)
 b2 = Blender("Blender2", 1990, 250)
 
 print(b2)
 b2.blend("cibule")
+print()
 
 mg1 = Meat_Grinder("Meat Grinder1 ", 2050, "Šnek")
 mg2 = Meat_Grinder("Meat Grinder 2", 3500, "nože")
 
 print(mg1)
 mg2.grind("telecí maso")
+print()
 
 
 #Task 2
@@ -32,11 +35,13 @@ f1 = Frigate("Frigata 1", 60, 10, 5)
 f2 = Frigate("Frigata 2", 40, 9, 3)
 print(f1.number_of_sails)
 print(f2)
+print()
 
 c1 = Cruiser("Křižník 1", 80, 20, "obranný")
 c2 = Cruiser("Křižník 2", 75, 15, "pancéřovaný")
 
 print(c2.type)
+print()
 
 d1 = Destroyer("Torpédoborec 1", 90, 10, "rakety")
 d2 = Destroyer("Torpédoborec 2", 80, 12, "torpéda")
--- pythonProject1/ship.py
+++ pythonProject1/ship.py
@@ -5,7 +5,7 @@ class Ship:
         self.width = width
 
     def __str__(self):
-        return f"Typ této lodi je {type(self)}. Její jméno je {self.name} a její rozměry jsou {self.length}m x {self.width}m."
+        return f"Typ této lodi je {type(self).__name__}. Její jméno je {self.name} a její rozměry jsou {self.length}m x {self.width}m."
 
     def destroy(self, other):
         print(f"Loď {other.name} byla zničena lodí {self.name}")
